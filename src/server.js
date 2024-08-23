9ef2e732bc6fd9f83e46f9f0598b140229c8c5e0
skillbridge-projects <SkillBridge-Projects@nextgen-is.com>
2024-08-23 16:42:15 +0530

    Demo base code

--- src/server.js
+++ src/server.js
@@ -3,10 +3,4 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 app.use(cors()); // Enable CORS to allow requests from your React app
-const names = ["nawaz shareef", "chinna", "moses", "javeed"];
-app.get("/api/names", (req, res) => {
-  res.json(names);
-});
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+
